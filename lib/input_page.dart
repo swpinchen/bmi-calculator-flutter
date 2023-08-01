26f02c75a0af4c19e1c4663a85ba33fa69a627ef
swpinchen <swpinchen@outlook.com>
2023-08-01 20:18:36 +0900

    Themes, refactor, custom widgets, GestureDetector

---
+++ lib/input_page.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
+
+const double bottomContainerHeight = 80.0;
+const Color bottomContainerColor = Color(0xFFEB1555);
+const Color inactiveCardColor = Color(0xFF111328);
+const Color activeCardColor = Color(0xFF1D1E33);
+
+enum Gender { male, female }
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  Gender? selectedGender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedGender = Gender.male;
+                        });
+                      },
+                      child: ReusableCard(
+                        colour: (selectedGender == Gender.male) ? activeCardColor : inactiveCardColor,
+                        cardChild: IconContent(
+                          icon: FontAwesomeIcons.mars,
+                          label: "MALE",
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                          setState(() {
+                            selectedGender = Gender.female;
+                          });
+                        },
+                      child: ReusableCard(
+                        colour: (selectedGender == Gender.female) ? activeCardColor : inactiveCardColor,
+                        cardChild: IconContent(
+                          icon: FontAwesomeIcons.venus,
+                          label: "FEMALE",
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(colour: activeCardColor),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(colour: activeCardColor),
+                  ),
+                  Expanded(
+                    child: ReusableCard(colour: activeCardColor),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: bottomContainerColor,
+              width: double.infinity,
+              height: bottomContainerHeight,
+            )
+          ],
+        )
+    );
+  }
+}
