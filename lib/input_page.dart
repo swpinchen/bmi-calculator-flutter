abf2f5d017743763b1581f780db3c023934245e7
swpinchen <swpinchen@outlook.com>
2023-08-02 12:15:08 +0900

    Refactored - moved onTap function to reusable class

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -30,34 +30,30 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: [
                   Expanded(
-                    child: GestureDetector(
-                      onTap: () {
+                    child: ReusableCard(
+                      onPress: () {
                         setState(() {
                           selectedGender = Gender.male;
                         });
                       },
-                      child: ReusableCard(
-                        colour: (selectedGender == Gender.male) ? activeCardColor : inactiveCardColor,
-                        cardChild: IconContent(
-                          icon: FontAwesomeIcons.mars,
-                          label: "MALE",
-                        ),
+                      colour: (selectedGender == Gender.male) ? activeCardColor : inactiveCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: "MALE",
                       ),
                     ),
                   ),
                   Expanded(
-                    child: GestureDetector(
-                      onTap: () {
-                          setState(() {
-                            selectedGender = Gender.female;
-                          });
-                        },
-                      child: ReusableCard(
-                        colour: (selectedGender == Gender.female) ? activeCardColor : inactiveCardColor,
-                        cardChild: IconContent(
-                          icon: FontAwesomeIcons.venus,
-                          label: "FEMALE",
-                        ),
+                    child: ReusableCard(
+                      onPress: () {
+                        setState(() {
+                          selectedGender = Gender.female;
+                        });
+                      },
+                      colour: (selectedGender == Gender.female) ? activeCardColor : inactiveCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: "FEMALE",
                       ),
                     ),
                   ),
@@ -65,16 +61,22 @@ class _InputPageState extends State<InputPage> {
               ),
             ),
             Expanded(
-              child: ReusableCard(colour: activeCardColor),
+              child: ReusableCard(
+                colour: activeCardColor,
+              ),
             ),
             Expanded(
               child: Row(
                 children: [
                   Expanded(
-                    child: ReusableCard(colour: activeCardColor),
+                    child: ReusableCard(
+                      colour: activeCardColor,
+                    ),
                   ),
                   Expanded(
-                    child: ReusableCard(colour: activeCardColor),
+                    child: ReusableCard(
+                      colour: activeCardColor,
+                    ),
                   ),
                 ],
               ),
--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.colour, this.cardChild});
+  ReusableCard({required this.colour, this.cardChild, this.onPress});
+  // Function passed in with onPress as be instance variable.
   final Color colour;
   final Widget? cardChild;
+  final void Function()? onPress;  // declares function with no parameters
+  // final Function onPress;       // declares any function - but will give error
+  // 'Function?' can't be assigned to the parameter type 'void Function()?
+  // void keyword can be included but is not mandatory
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: colour
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: cardChild,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: colour
+        ),
       ),
     );
   }
